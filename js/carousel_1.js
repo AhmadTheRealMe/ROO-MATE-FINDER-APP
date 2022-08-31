cf8bf8d2c7887b2a02cca90c9cb99010774711f9
Ahmad Olumo <87186014+AhmadTheRealMe@users.noreply.github.com>
2022-08-31 10:45:41 +0100

    Add files via upload

---
+++ js/carousel_1.js
@@ -0,0 +1,109 @@
+/*
+ *	This content is generated from the PSD File Info.
+ *	(Alt+Shift+Ctrl+I).
+ *
+ *	@desc 		
+ *	@file 		carousel_1
+ *	@date 		Wednesday 31st of August 2022 05:56:25 AM
+ *	@title 		DESIGN
+ *	@author 	
+ *	@keywords 	
+ *	@generator 	Export Kit v1.3.figma
+ *
+*/
+
+var content_container = document.createElement("div");
+content_container.style.width = "414px";
+content_container.id = "content_container";
+content_container.style.position = "relative";
+content_container.style.display = "block";
+content_container.style.margin = "0 auto";
+document.body.appendChild(content_container);
+
+var _bg__carousel_1 = document.createElement("div");
+_bg__carousel_1.id = "_bg__carousel_1";
+_bg__carousel_1.style.left = "0px";
+_bg__carousel_1.style.top = "0px";
+_bg__carousel_1.style.width = "414px";
+_bg__carousel_1.style.height = "826px";
+_bg__carousel_1.style.background = 'rgba(29,137,184,1)';
+
+content_container.appendChild(_bg__carousel_1);
+
+var skip = document.createElement("div");
+skip.innerHTML = "Skip";
+skip.style.fontWeight = "bold";
+skip.style.textAlign = "center";
+skip.id = "skip";
+skip.style.left = "337px";
+skip.style.top = "59px";
+skip.style.width = "54px";
+skip.style.height = "40px";
+skip.style.fontFamily = "Poppins";
+skip.style.fontSize = "18px";
+skip.style.overflow = "hidden";
+skip.style.color = "#F9FDFF";
+
+content_container.appendChild(skip);
+
+var rectangle_1 = document.createElement("div");
+rectangle_1.id = "rectangle_1";
+rectangle_1.style.left = "18px";
+rectangle_1.style.top = "654px";
+rectangle_1.style.width = "34px";
+rectangle_1.style.height = "7px";
+rectangle_1.style.background = 'rgba(224.19,32.69,170.57,1)';
+
+content_container.appendChild(rectangle_1);
+
+var rectangle_2 = document.createElement("div");
+rectangle_2.id = "rectangle_2";
+rectangle_2.style.left = "60px";
+rectangle_2.style.top = "654px";
+rectangle_2.style.width = "34px";
+rectangle_2.style.height = "7px";
+rectangle_2.style.background = 'rgba(217,217,217,1)';
+
+content_container.appendChild(rectangle_2);
+
+var rectangle_3 = document.createElement("div");
+rectangle_3.id = "rectangle_3";
+rectangle_3.style.left = "102px";
+rectangle_3.style.top = "654px";
+rectangle_3.style.width = "34px";
+rectangle_3.style.height = "7px";
+rectangle_3.style.background = 'rgba(217,217,217,1)';
+
+content_container.appendChild(rectangle_3);
+
+var rectangle_4 = document.createElement("div");
+rectangle_4.id = "rectangle_4";
+rectangle_4.style.left = "313px";
+rectangle_4.style.top = "731px";
+rectangle_4.style.width = "64px";
+rectangle_4.style.height = "35px";
+rectangle_4.style.borderRadius = "35px";
+rectangle_4.style.background = 'rgba(255,255,255,1)';
+
+content_container.appendChild(rectangle_4);
+
+var arrow_1 = document.createElement("img");
+arrow_1.id = "arrow_1";
+arrow_1.style.left = "335px";
+arrow_1.style.top = "749px";
+arrow_1.style.width = "20px";
+arrow_1.style.height = "0px";
+arrow_1.src = "skins/arrow_1.png";
+
+content_container.appendChild(arrow_1);
+
+var online_party = document.createElement("img");
+online_party.id = "online_party";
+online_party.style.left = "16px";
+online_party.style.top = "165px";
+online_party.style.width = "373px";
+online_party.style.height = "373px";
+online_party.src = "skins/online_party.png";
+
+content_container.appendChild(online_party);
+
